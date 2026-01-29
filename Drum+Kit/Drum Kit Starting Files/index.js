112e645b50ec7845503a9b4b92b672248aa3deff
Gabriel Moura <gabriel-moura22@hotmail.com>
2026-01-29 17:14:08 -0300

    Add files via upload

--- Drum+Kit/Drum Kit Starting Files/index.js
+++ Drum+Kit/Drum Kit Starting Files/index.js
@@ -1,46 +1,61 @@
+// Detectar clique do mouse
+
 var numeroDeBaterias = document.querySelectorAll(".bateria").length;
 
 for (var i = 0; i < numeroDeBaterias; i++) {
-  document.querySelectorAll(".bateria")[i].addEventListener("click", function(){
+  document.querySelectorAll(".bateria")[i].addEventListener("click", function() {
     
     var letraBateria = this.innerHTML;
-    switch (letraBateria) {
+
+    executarSom(letraBateria);
+  });
+}
+
+// Detectar letra do teclado pressionado
+
+document.addEventListener("keydown", function(event) {
+
+    executarSom(event.key);
+
+});
+
+function executarSom(tecla) {
+
+  switch (tecla) {
       case "w":
         var crashAudio = new Audio('./sounds/crash.mp3');
         crashAudio.play();
         break;
+
       case "a":
         var kickBassAudio = new Audio('sounds/kick-bass.mp3');
         kickBassAudio.play();
         break;
+
       case "s":
         var snareAudio = new Audio('sounds/snare.mp3');
         snareAudio.play();
         break;
+
       case "d":
         var tom1 = new Audio('sounds/tom-1.mp3');
         tom1.play();
         break;
+
       case "j":
         var tom2 = new Audio('sounds/tom-2.mp3');
         tom2.play();
         break;
+
       case "k":
         var tom3 = new Audio('sounds/tom-3.mp3');
         tom3.play();
         break;
+        
       case "l":
         var tom4 = new Audio('sounds/tom-4.mp3');
         tom4.play();
         break;
 
       default: console.log(letraBateria);
-    }
-  });
-}
-
-
-
-
-// var audio = new Audio('sounds/tom-1.mp3');
-//     audio.play();
+    }};
