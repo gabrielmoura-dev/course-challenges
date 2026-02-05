6fa46040500ae1a2b21d8a5e47a56a1ba9b342aa
Gabriel Moura <gabriel-moura22@hotmail.com>
2026-02-05 17:39:35 -0300

    weekMoney function to totalHercyMoney problem

--- LeetCode/1716. Calculate Money in Leetcode Bank/index.js
+++ LeetCode/1716. Calculate Money in Leetcode Bank/index.js
@@ -1,12 +1,15 @@
-function weekMoney () {
-    let resultMoney = [];
-    
-    for (i = 1; i < 8; i++) {
-        resultMoney.push(i);
-    }
+var totalHercyMoney = function(n) {
 
-    return resultMoney;
-}
-
-const weekValues = weekMoney();
-console.log(weekValues);
+    // Semanas completas que cabem em n
+    const completedWeeks = Math.floor(n / 7);
+    // Dias restantes
+    const remainingDays = n % 7;
+    // Soma base de uma semana. Ou seja, 1+2+3+4+5+6+7 = 28
+    const sumWeek = 28;
+    // Progressão Aritmética para encontrar o total de semanas
+    const totalWeeks = completedWeeks * sumWeek + 7 * (completedWeeks * (completedWeeks - 1)) / 2;
+    // Incrementação diária 
+    const remainingTotal = remainingDays * completedWeeks + (remainingDays * (remainingDays + 1)) / 2;
+    // Total de semanas + total de dias restantes 
+    return totalWeeks + remainingTotal;
+};
